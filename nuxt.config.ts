33aa02533417e8e3abe59180c6351cd7d1846b8a
i99dev <obaid.altuniji@gmail.com>
2023-02-07 07:15:11 +0400

    fix tailwindcss issue 🪲

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    configPath: 'tailwind.config',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  }
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,21 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
+const srcDir = "..";
+
 export default <Partial<Config>>{
+  content: [
+    "components/**/*.{vue,js,ts}",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "composables/**/*.{js,ts}",
+    "plugins/**/*.{js,ts}",
+    "App.{js,ts,vue}",
+    "app.{js,ts,vue}",
+    "Error.{js,ts,vue}",
+    "error.{js,ts,vue}",
+    "content/**/*.md",
+  ],
   theme: {
     extend: {
       colors: {},
